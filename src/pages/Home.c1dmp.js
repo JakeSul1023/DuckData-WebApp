c13a8ab0fc18cfe642ead86d96a42bb64b169626
Jacob Sullivan <jakesullivan1023@gmail.com>
2025-01-22 21:27:58 -0600

    test

--- src/pages/Home.c1dmp.js
+++ src/pages/Home.c1dmp.js
@@ -1,6 +1,20 @@
 // API Reference: https://www.wix.com/velo/reference/api-overview/introduction
 // “Hello, World!” Example: https://learn-code.wix.com/en/article/hello-world
 
-$w.onReady(function () {
+//$w.onReady(function () {
     //Jake sucks
-});
+//});
+
+
+//initializing the map and setting its view
+const map = L.map('map').setView([51.505, -0.09], 13); // Latitude, Longitude, Zoom level
+
+// Add a tile layer (OpenStreetMap in this case)
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19, // Maximum zoom level
+  attribution: '© OpenStreetMap contributors',
+}).addTo(map);
+
+// Add a marker with a popup
+const marker = L.marker([51.505, -0.09]).addTo(map);
+marker.bindPopup('Hello, Leaflet!').openPopup();
